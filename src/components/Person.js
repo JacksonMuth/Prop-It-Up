5b5cf0dbc68fd0278e5bec795b67cf499aa09899
unknown <muth.jackson@yahoo.com>
2020-07-09 15:39:42 -0700

    added state

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            FirstName: this.props.FirstName,
+            LastName: this.props.LastName,
+            Age: this.props.Age,
+            Hair: this.props.Hair
+        };
+    }
+    
     render() {
-        const { FirstName, LastName, Age, Hair } = this.props;
+        const { FirstName, LastName, Age, Hair } = this.state;
         return (
             <div>
                 <h1>{ LastName }, { FirstName }</h1>
                 <p>Age: { Age }</p>
                 <p>Hair Color: { Hair }</p>
+                <button onClick={ () => { this.setState({ Age: this.state.Age + 1 })} }>Birthday Button for {FirstName} {LastName}</button>
             </div>
         );
     }
